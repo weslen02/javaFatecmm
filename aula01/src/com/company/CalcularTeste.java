f3758302f614e6f83b5a78b5066d30a91648a0f0
weslen almeida <weslen-a@hotmail.com>
2019-08-20 21:14:11 -0300

    aula 1 - 2 e 3

--- aula01/src/com/company/CalcularTeste.java
+++ aula01/src/com/company/CalcularTeste.java
@@ -1,8 +1,18 @@
 package com.company;
 
-public class Main {
+public class CalcularTeste {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //write your code here
+
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.nome = "chico";
+        pessoa1.peso = 110;
+        pessoa1.altura = 1.72;
+        pessoa1.idade = 52;
+
+        System.out.printf("IMC do %s%né: %f%n%nDados da pessoa" ,pessoa1.nome, pessoa1.calcularImc());
+        pessoa1.imprimir();
     }
 }
---
+++ aula03/prejetoEntrada/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner leitor = new Scanner(System.in);
+        System.out.printf("Digite o nome do sujeito:%n");
+        String nome = leitor.nextLine();
+        System.out.println("nome digitado = " + nome);
+        //exemplos com conversao de tipo
+        int a;
+        double b;
+        System.out.println("Digite um valor para a: ");
+        a = Integer.parseInt(leitor.nextLine());
+        System.out.println("Digite um valor para b: ");
+        Locale.setDefault(Locale.US);
+        b = Double.parseDouble(leitor.nextLine());
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        double c = Double.parseDouble("356.89f");
+    }
+}
